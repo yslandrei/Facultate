375315f9e7bc73dad5a506720f6b1c7407bde62b
yslandrei <andreidiaconescu@gmail.com>
2022-10-23 15:55:25 +0300

    update

--- personalProjects/chess/gui.py
+++ personalProjects/chess/gui.py
@@ -1,13 +1,30 @@
 import pygame as p
 
-
-WIDTH = HEIGHT = 800
+WIDTH = HEIGHT = 720
 DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15
 IMAGES = {}
 
-def loadPieces():
+def loadImages():
     pieces = ["bP", "bR", "bH", "bB", "bQ", "bK", "wP", "wR", "wH", "wB", "wQ", "wK"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+def drawGamestate(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r + c) % 2]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
--- personalProjects/chess/main.py
+++ personalProjects/chess/main.py
@@ -1,8 +1,22 @@
-from gui import p, loadPieces
+from gameState import gameState
+from gui import drawGamestate, p, loadImages, WIDTH, HEIGHT, MAX_FPS
 
 def main():
     p.init()
-    loadPieces()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = gameState()
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGamestate(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
 
 if __name__ == "__main__":
     main()
