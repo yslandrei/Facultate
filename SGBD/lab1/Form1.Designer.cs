af0b911211fbd0a25c83b9241340f6d71a306cf9
Andrei Diaconescu <andreidiaconescu2003@gmail.com>
2024-03-17 19:49:01 +0200

    update

---
+++ SGBD/lab1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            dataGridView1 = new DataGridView();
+            buttonFirst = new Button();
+            buttonPrev = new Button();
+            buttonNext = new Button();
+            buttonLast = new Button();
+            labelRecord = new Label();
+            dataGridChildView = new DataGridView();
+            labelName = new Label();
+            labelPrice = new Label();
+            textBoxName = new TextBox();
+            textBoxPrice = new TextBox();
+            buttonAdd = new Button();
+            buttonDelete = new Button();
+            buttonUpdate = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridChildView).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(23, 25);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(343, 342);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // buttonFirst
+            // 
+            buttonFirst.Location = new Point(30, 373);
+            buttonFirst.Name = "buttonFirst";
+            buttonFirst.Size = new Size(75, 23);
+            buttonFirst.TabIndex = 1;
+            buttonFirst.Text = "First";
+            buttonFirst.UseVisualStyleBackColor = true;
+            buttonFirst.Click += buttonFirst_Click;
+            // 
+            // buttonPrev
+            // 
+            buttonPrev.Location = new Point(111, 373);
+            buttonPrev.Name = "buttonPrev";
+            buttonPrev.Size = new Size(75, 23);
+            buttonPrev.TabIndex = 2;
+            buttonPrev.Text = "Previous";
+            buttonPrev.UseVisualStyleBackColor = true;
+            buttonPrev.Click += buttonPrev_Click;
+            // 
+            // buttonNext
+            // 
+            buttonNext.Location = new Point(192, 373);
+            buttonNext.Name = "buttonNext";
+            buttonNext.Size = new Size(75, 23);
+            buttonNext.TabIndex = 3;
+            buttonNext.Text = "Next";
+            buttonNext.UseVisualStyleBackColor = true;
+            buttonNext.Click += buttonNext_Click;
+            // 
+            // buttonLast
+            // 
+            buttonLast.Location = new Point(273, 373);
+            buttonLast.Name = "buttonLast";
+            buttonLast.Size = new Size(75, 23);
+            buttonLast.TabIndex = 4;
+            buttonLast.Text = "Last";
+            buttonLast.UseVisualStyleBackColor = true;
+            buttonLast.Click += buttonLast_Click;
+            // 
+            // labelRecord
+            // 
+            labelRecord.AutoSize = true;
+            labelRecord.Location = new Point(23, 7);
+            labelRecord.Name = "labelRecord";
+            labelRecord.Size = new Size(44, 15);
+            labelRecord.TabIndex = 5;
+            labelRecord.Text = "Record";
+            // 
+            // dataGridChildView
+            // 
+            dataGridChildView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridChildView.Location = new Point(435, 25);
+            dataGridChildView.Name = "dataGridChildView";
+            dataGridChildView.Size = new Size(343, 181);
+            dataGridChildView.TabIndex = 6;
+            dataGridChildView.CellClick += dataGridChildView_CellClick;
+            // 
+            // labelName
+            // 
+            labelName.AutoSize = true;
+            labelName.Location = new Point(448, 215);
+            labelName.Name = "labelName";
+            labelName.Size = new Size(39, 15);
+            labelName.TabIndex = 7;
+            labelName.Text = "Name";
+            // 
+            // labelPrice
+            // 
+            labelPrice.AutoSize = true;
+            labelPrice.Location = new Point(615, 215);
+            labelPrice.Name = "labelPrice";
+            labelPrice.Size = new Size(33, 15);
+            labelPrice.TabIndex = 8;
+            labelPrice.Text = "Price";
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(493, 212);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(100, 23);
+            textBoxName.TabIndex = 9;
+            // 
+            // textBoxPrice
+            // 
+            textBoxPrice.Location = new Point(654, 212);
+            textBoxPrice.Name = "textBoxPrice";
+            textBoxPrice.Size = new Size(100, 23);
+            textBoxPrice.TabIndex = 10;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Location = new Point(448, 241);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(75, 23);
+            buttonAdd.TabIndex = 11;
+            buttonAdd.Text = "Add";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += buttonAdd_Click;
+            // 
+            // buttonDelete
+            // 
+            buttonDelete.Location = new Point(679, 241);
+            buttonDelete.Name = "buttonDelete";
+            buttonDelete.Size = new Size(75, 23);
+            buttonDelete.TabIndex = 12;
+            buttonDelete.Text = "Delete";
+            buttonDelete.UseVisualStyleBackColor = true;
+            buttonDelete.Click += buttonDelete_Click;
+            // 
+            // buttonUpdate
+            // 
+            buttonUpdate.Location = new Point(564, 241);
+            buttonUpdate.Name = "buttonUpdate";
+            buttonUpdate.Size = new Size(75, 23);
+            buttonUpdate.TabIndex = 13;
+            buttonUpdate.Text = "Update";
+            buttonUpdate.UseVisualStyleBackColor = true;
+            buttonUpdate.Click += buttonUpdate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonUpdate);
+            Controls.Add(buttonDelete);
+            Controls.Add(buttonAdd);
+            Controls.Add(textBoxPrice);
+            Controls.Add(textBoxName);
+            Controls.Add(labelPrice);
+            Controls.Add(labelName);
+            Controls.Add(dataGridChildView);
+            Controls.Add(labelRecord);
+            Controls.Add(buttonLast);
+            Controls.Add(buttonNext);
+            Controls.Add(buttonPrev);
+            Controls.Add(buttonFirst);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridChildView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private DataGridView dataGridView1;
+        private Button buttonFirst;
+        private Button buttonPrev;
+        private Button buttonNext;
+        private Button buttonLast;
+        private Label labelRecord;
+        private DataGridView dataGridChildView;
+        private Label labelName;
+        private Label labelPrice;
+        private TextBox textBoxName;
+        private TextBox textBoxPrice;
+        private Button buttonAdd;
+        private Button buttonDelete;
+        private Button buttonUpdate;
+    }
+}
---
+++ SGBD/lab1/Form1.cs
@@ -0,0 +1,162 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace lab1
+    {
+    public partial class Form1 : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source=YSLANDREI\\SQLEXPRESS; Initial Catalog = ebay_db_manual; Integrated Security = True");
+        SqlDataAdapter da = new SqlDataAdapter();
+
+        DataSet ds = new DataSet();
+        BindingSource bs = new BindingSource();
+
+        DataSet dsChild = new DataSet();
+        BindingSource bsChild = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("SELECT id, name, email FROM seller", cs);
+            ds.Clear();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            bs.DataSource = ds.Tables[0];
+
+            int seller_id = int.Parse(dataGridView1.Rows[0].Cells[0].Value.ToString());
+            da.SelectCommand = new SqlCommand($"SELECT id, name, price FROM product WHERE product.seller_id = {seller_id}", cs);
+            dsChild.Clear();
+            da.Fill(dsChild);
+            dataGridChildView.DataSource = dsChild.Tables[0];
+            bsChild.DataSource = dsChild.Tables[0];
+            textBoxName.Text = dataGridChildView.Rows[0].Cells[1].Value.ToString();
+            textBoxPrice.Text = dataGridChildView.Rows[0].Cells[2].Value.ToString();
+
+            labelRecord.Text = "Record " + (bs.Position + 1) + " of " + bs.Count;
+        }
+
+        private void records()
+        {
+            labelRecord.Text = "Record " + (bs.Position + 1) + " of " + bs.Count;
+        }
+
+        private void dataGridView1Update()
+        {
+            dataGridView1.ClearSelection();
+            dataGridView1.Rows[bs.Position].Selected = true;
+            records();
+            dataGridChildViewUpdate();
+        }
+
+        private void dataGridChildViewUpdate()
+        {
+            int seller_id = int.Parse(dataGridView1.SelectedRows[0].Cells[0].Value.ToString());
+            da.SelectCommand = new SqlCommand($"SELECT id, name, price FROM product WHERE product.seller_id = {seller_id}", cs);
+            dsChild.Clear();
+            da.Fill(dsChild);
+            dataGridChildView.DataSource = dsChild.Tables[0];
+            bsChild.DataSource = dsChild.Tables[0];
+            textBoxName.Text = dataGridChildView.Rows[0].Cells[1].Value.ToString();
+            textBoxPrice.Text = dataGridChildView.Rows[0].Cells[2].Value.ToString();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.ClearSelection();
+            bs.Position = e.RowIndex;
+            dataGridView1.Rows[e.RowIndex].Selected = true;
+            dataGridChildViewUpdate();
+            records();
+        }
+
+        private void dataGridChildView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridChildView.ClearSelection();
+            bsChild.Position = e.RowIndex;
+            dataGridChildView.Rows[e.RowIndex].Selected = true;
+            textBoxName.Text = dataGridChildView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBoxPrice.Text = dataGridChildView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            //recordsChild();
+        }
+
+
+        private void buttonFirst_Click(object sender, EventArgs e)
+        {
+            bs.MoveFirst();
+            dataGridView1Update();
+            dataGridChildViewUpdate();
+            records();
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            bs.MovePrevious();
+            dataGridView1Update();
+            dataGridChildViewUpdate();
+            records();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            bs.MoveNext();
+            dataGridView1Update();
+            dataGridChildViewUpdate();
+            records();
+        }
+
+        private void buttonLast_Click(object sender, EventArgs e)
+        {
+            bs.MoveLast();
+            dataGridView1Update();
+            dataGridChildViewUpdate();
+            records();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            da.InsertCommand = new SqlCommand("insert into product (name, created_date, seller_id, category_id, price) values (@n, @c, @sid, @cid, @p)", cs);
+            da.InsertCommand.Parameters.Add("@n", SqlDbType.VarChar).Value = textBoxName.Text;
+            da.InsertCommand.Parameters.Add("@c", SqlDbType.DateTime).Value = DateTime.Now;
+            da.InsertCommand.Parameters.Add("@sid", SqlDbType.BigInt).Value = int.Parse(ds.Tables[0].Rows[bs.Position][0].ToString());
+            da.InsertCommand.Parameters.Add("@cid", SqlDbType.BigInt).Value = 1;
+            da.InsertCommand.Parameters.Add("@p", SqlDbType.Money).Value = textBoxPrice.Text;
+            cs.Open();
+            da.InsertCommand.ExecuteNonQuery();
+            cs.Close();
+            dsChild.Clear();
+            da.Fill(dsChild);
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            da.UpdateCommand = new SqlCommand("update product set name = @n, price = @p where id = @id", cs);
+            da.UpdateCommand.Parameters.Add("@n", SqlDbType.VarChar).Value = textBoxName.Text;
+            da.UpdateCommand.Parameters.Add("@p", SqlDbType.Money).Value = textBoxPrice.Text;
+            da.UpdateCommand.Parameters.Add("@id", SqlDbType.Int).Value = dsChild.Tables[0].Rows[bsChild.Position][0];
+            cs.Open();
+            da.UpdateCommand.ExecuteNonQuery();
+            cs.Close();
+            dsChild.Clear();
+            da.Fill(dsChild);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            da.DeleteCommand = new SqlCommand("delete from product where ID = @id", cs);
+            da.DeleteCommand.Parameters.Add("@id", SqlDbType.Int).Value = dsChild.Tables[0].Rows[bsChild.Position][0];
+            cs.Open();
+            da.DeleteCommand.ExecuteNonQuery();
+            cs.Close();
+            dsChild.Clear();
+            da.Fill(dsChild);
+        }
+    }
+}
